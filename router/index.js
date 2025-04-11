9efa2f5443614c376d66a0e0633e589193819334
goldstar0417 <goldstar0417@proton.me>
2025-04-11 07:36:02 -0400

    complete community info api

--- router/index.js
+++ router/index.js
@@ -3,5 +3,6 @@ const Resource = require('./resource');
 
 //resource api
 router.get("/resource/init", Resource.initializeMapResource);
+router.get("/resource/createcommunity", Resource.initializeCommunityInfomation);
 
 module.exports = router;
--- router/resource/index.js
+++ router/resource/index.js
@@ -28,3 +28,22 @@ exports.initializeMapResource = async (req, res) => {
 
     res.json({data: productList, status: true, type: "InitMap"});
 }
+
+exports.initializeCommunityInfomation = async (req, res) => {
+    let communities = [
+        {
+            name: "Farmer's villege",
+            produce: "farmer",
+            demandTileCount: 4,
+            points: [{x: 11, y: 13}, {x: 12, y: 13}, {x:12, y:14}, {x:13, y:13}]
+        },
+        {
+            name: "Fisherman's villege",
+            produce: "fisherman",
+            demandTileCount: 3,
+            points: [{x:21, y:7}, {x:22, y:7}, {x:22, y:8}]
+        }
+    ];
+
+    res.json({data: communities, status: true, type: "CreateCommunity"});
+}
